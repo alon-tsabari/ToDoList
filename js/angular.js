7a4e9eb8b55899ef78f8de000789ea6a665d2041
alon-tsabari <alon6820@gmail.com>
2016-11-12 20:57:18 +0200

    Update angular.js

--- js/angular.js
+++ js/angular.js
@@ -27,7 +27,7 @@ var app = angular.module('toDoList', [])
 .service('dataService', function($http) {
 	
   this.getTodos = function(callback) {
-    $http.get('vendor/data.json').then(callback);
+    $http.get('js/data.json').then(callback);
   };
   
   this.deleteTodo = function(todo) {
