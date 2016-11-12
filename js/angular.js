d02af88f8e4efcb29e40ba4b261fbb258417c83e
alon-tsabari <alon6820@gmail.com>
2016-11-12 03:15:11 +0200

    Update angular.js

--- js/angular.js
+++ js/angular.js
@@ -1,18 +1,17 @@
 var app = angular.module('toDoList', [])
 
-.controller('mainCtrl', function($scope, dataServices) {
+.controller('mainCtrl', function($scope, dataService) {
 
 	$scope.addTask = function() {
 		this.todos.push({});
 	}
 
-	$scope.todos = dataServices.getTodos();
+	$scope.todos = dataService.getTodos();
 })
 
-.service('dataServices', function($http) {
-
+.service('dataService', function($http) {
 	this.getTodos = $http.get('js/data.json').then(function(response) {
 		console.log(response.data);
 		return response.data;
-	});
-});
+	})
+})
